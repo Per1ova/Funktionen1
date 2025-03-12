597c97b00c147ebe5885d170dc48f53433d2e078
Per1ova <azzabimourad2@gmail.com>
2025-03-12 15:54:39 +0100

    Main

---
+++ aufgabe1.dart
@@ -0,0 +1,9 @@
+void printNumber(int number) {
+  print(number);
+}
+
+void main() {
+  printNumber(5);
+  printNumber(10);
+  printNumber(42);
+}
---
+++ aufgabe2.dart
@@ -0,0 +1,11 @@
+void triplePrint(String text) {
+  for (int i = 0; i < 3; i++) {
+    print(text);
+  }
+}
+
+void main() {
+  triplePrint("Good Morning");
+  triplePrint("Today is good day");
+  triplePrint("Weekend is soon.");
+}
